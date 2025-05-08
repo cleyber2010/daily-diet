b02d4c67930cf63e848fbf94d89aeb5960503e73
cleyber <cleyber.fernandes@gmail.com>
2025-05-08 16:00:22 -0300

    alter snackRoutes

--- src/routes/snackRoutes.ts
+++ src/routes/snackRoutes.ts
@@ -3,14 +3,10 @@ import {checkLogin} from "../middlewares/check-login";
 import {knexDb} from "../database";
 
 export async function snackRoutes(app: FastifyInstance) {
-
-
     app.addHook('preHandler', checkLogin);
 
     app.get('/', (request, reply) => {
         return "Get snacks"
-    app.get('/', async (request, reply) => {
-        return knexDb('sqlite_schema').select("*")
     });
 
     app.post('/', (request, reply) => {
